ec3b0e80cd09239f637e6aca52f9ac767609f330
Made Raditya Wijaya <medeputu292@gmail.com>
2025-04-19 13:24:40 +0700

    fix index.php

--- index.php
+++ index.php
@@ -3,13 +3,14 @@ session_start();
 
 if (isset($_SESSION['user_id'])) {
     if ($_SESSION['role'] == 'admin') {
-        header("Location: pages/dashboard.php");
+        header("Location: pages/admin/dashboard.php");
     } elseif ($_SESSION['role'] == 'kasir') {
         header("Location: pages/order_list.php");
     } elseif ($_SESSION['role'] == 'pelayan') {
         header("Location: pages/menu_list.php");
     }
-} else {
+}
+else {
     header("Location: pages/login.php");
 }
 exit();
